764f3231013bcca4c518a7e59f7144a8e000bdaa
Saurabh Kumar <146050167+Saurabh-Kumar20-24@users.noreply.github.com>
2025-04-10 11:18:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let currentTime=document.getElementById("timer");
+let currentDate= new Date();
+
+setInterval(
+	function(){
+		currentDate=new Date();
+		currentTime.innerHTML=currentDate.toLocaleString();
+	},1000
+)
